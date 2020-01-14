494a664641d953032b89de2c1ce842f44e10efdb
Marian Priebe <m.k.priebe@qmul.ac.uk>
2020-01-14 16:42:03 +0000

    added Guy and Alicja

--- randomize.R
+++ randomize.R
@@ -1,6 +1,6 @@
 
 
-people.raw <- "Priyam, Yannick, Marian, Magdalena, Federico, Raphaella, Anindita, Gabriel, Raph, Iwo"
+people.raw <- "Priyam, Yannick, Marian, Magdalena, Federico, Raphaella, Anindita, Gabriel, Raph, Iwo, Guy, Alicja"
 
 journals.raw <- "Mol Ecol, Plos Genet, PNAS, Science, Nature, Nature Genetics,
                  Plos Biol, Plos Comp Biol, eLife, Insectes Sociaux, MBE, Cell,
